9158ad19ee0499b1e26b97b4243e24d795ecac93
Andrea Passante <zante@inwind.it>
2013-09-14 21:46:33 +0100

    Fixed DB settings

--- www/class/impostazioni.php
+++ www/class/impostazioni.php
@@ -5,26 +5,26 @@
 		
 		static $DBserver = array(
 			'my.blog'					=> 'localhost',
-			'www.strangerinlondon.com'	=> 'hostingmysql75.register.it',
-			'strangerinlondon.com'		=> 'hostingmysql75.register.it'
+			'www.strangerinlondon.com'	=> 'localhost',
+			'strangerinlondon.com'		=> 'localhost'
 		);
 		
 		static $DBuser = array(
 			'my.blog'					=> 'root',
-			'www.strangerinlondon.com'	=> 'AP20772_blog',
-			'strangerinlondon.com'		=> 'AP20772_blog'
+			'www.strangerinlondon.com'	=> 'root',
+			'strangerinlondon.com'		=> 'root'
 		);
 		
 		static $DBpassword = array(
-			'my.blog'					=> '',
-			'www.strangerinlondon.com'	=> 'antani',
-			'strangerinlondon.com'		=> 'antani'
+			'my.blog'					=> 'antani75',
+			'www.strangerinlondon.com'	=> 'antani75',
+			'strangerinlondon.com'		=> 'antani75'
 		);
 		
 		static $DBname = array(
-			'my.blog'					=> 'blog',
-			'www.strangerinlondon.com'	=> 'strangerinlondon_com_db',
-			'strangerinlondon.com'		=> 'strangerinlondon_com_db'
+			'my.blog'					=> 'strangerinlondon',
+			'www.strangerinlondon.com'	=> 'strangerinlondon',
+			'strangerinlondon.com'		=> 'strangerinlondon'
 		);
 
 	}
